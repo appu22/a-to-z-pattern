7f76d21fdae2bdb90a16f190bebf4b8de5322e07
appu-workz <appu.xworkz@gmail.com>
2021-02-01 19:18:06 +0530

    using Method Overriding

--- Temple.java
+++ Temple.java
@@ -4,10 +4,17 @@ class Temple
 	
 	{
 		puja();
+		puja("Ganesh");
 	}
 	public static void puja()
 	{
 		String timings="6 to 8";
 		System.out.println("Puja timings:"+ timings);
+	}
+	//method overriding 
+	public static void puja(String name)
+	{
+		String timings="9 to 8";
+		System.out.println("Puja timings :"+ timings+"LOrd Ganesh Temple Malleshwaram Bnglr"+name);
 	}
 }
