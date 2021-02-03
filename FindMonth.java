96c6eff3f494a186603a8dcfa453f7e77ecdff2a
appu-workz <appu.xworkz@gmail.com>
2021-02-03 21:29:28 +0530

    using conditional statements

---
+++ FindMonth.java
@@ -0,0 +1,51 @@
+class FindMonth
+{
+	public static void main(String v[])
+	{
+		int n=2;
+		if(n==1)
+		{
+			System.out.println("jan");
+		}
+		else if(n==2)
+		{
+			System.out.println("feb");
+		}
+		else if(n==3)
+		{
+			System.out.println("march");
+		}
+		else if(n==4)
+		{
+			System.out.println("April");
+		}
+		else if(n==5)
+		{
+			System.out.println("may");
+		}else if(n==6)
+		{
+			System.out.println("jun");
+		}else if(n==7)
+		{
+			System.out.println("jul");
+		}else if(n==8)
+		{
+			System.out.println("augst");
+		}else if(n==9)
+		{
+			System.out.println("sep");
+		}else if(n==10)
+		{
+			System.out.println("oct");
+		}else if(n==11)
+		{
+			System.out.println("nov");
+		}else if(n==12)
+		{
+			System.out.println("dec");
+		}
+		else
+			System.out.println("Only  12 months ");
+		
+	}
+}
